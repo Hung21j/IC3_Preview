08aa7da3b0a5a0b1784eb4c8d51edb21bf404906
Hung21j <95290071+Hung21j@users.noreply.github.com>
2026-09-20 20:16:35 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/IC3_Preview/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
